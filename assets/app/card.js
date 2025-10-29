80afa8f26e380f91bbdeb6c7fbdbdf848c63d1e3
piripirikebap <ttsoulv15@gmail.com>
2025-10-29 19:35:36 +0100

    Update card.js

--- assets/app/card.js
+++ assets/app/card.js
@@ -1,23 +1,14 @@
-
 var confirmElement = document.querySelector(".confirm");
-
 var time = document.getElementById("time");
-
-if (localStorage.getItem("update") == null){
-    localStorage.setItem("update", "24.12.2024")
-}
 
 var date = new Date();
 
-var dataReloadEvent = (data) => {
-    loadReadyData(data);
-}
-
-var imageReloadEvent = (image) => {
-    setImage(image);
-}
-
 var updateText = document.querySelector(".bottom_update_value");
+
+// Ustawienie daty ostatniej aktualizacji – nadal można użyć localStorage tylko dla update
+if (localStorage.getItem("update") == null){
+    localStorage.setItem("update", "24.12.2024")
+}
 updateText.innerHTML = localStorage.getItem("update");
 
 var update = document.querySelector(".update");
@@ -25,8 +16,7 @@ update.addEventListener('click', () => {
     var newDate = date.toLocaleDateString("pl-PL", options);
     localStorage.setItem("update", newDate);
     updateText.innerHTML = newDate;
-
-    scroll(0, 0)
+    scroll(0, 0);
 });
 
 setClock();
@@ -40,63 +30,63 @@ function setClock(){
 
 var unfold = document.querySelector(".info_holder");
 unfold.addEventListener('click', () => {
+    unfold.classList.toggle("unfolded");
+});
 
-    if (unfold.classList.contains("unfolded")){
-      unfold.classList.remove("unfolded");
-    }else{
-      unfold.classList.add("unfolded");
-    }
+// Funkcja pobierająca dane z URL
+function getQueryParams() {
+    const params = {};
+    window.location.search.substring(1).split("&").forEach(pair => {
+        const [key, value] = pair.split("=");
+        if (key) params[key] = decodeURIComponent(value.replace(/\+/g, " "));
+    });
+    return params;
+}
 
-})
+// Funkcja ustawiająca dane na stronie
+function loadReadyDataFromURL(){
+    const query = getQueryParams();
 
-function loadReadyData(result){
-    Object.keys(result).forEach((key) => {
-      result[key] = htmlEncode(result[key])
-    })
-    
-    var sex = result['sex'];
-    
-    var textSex;
-    if (sex === "m"){
-        textSex = "Mężczyzna"
-    }else if (sex === "k"){
-        textSex = "Kobieta"
-    }
+    // Kod zabezpieczający – jeśli brak jakiegoś parametru, wpisujemy pusty string
+    const safe = key => query[key] ? query[key] : "";
 
-    setData('seriesAndNumber', localStorage.getItem('seriesAndNumber'));
-    setData("name", result['name'].toUpperCase());
-    setData("surname", result['surname'].toUpperCase());
-    setData("nationality", result['nationality'].toUpperCase());
-    setData("fathersName", result['fathersName'].toUpperCase());
-    setData("mothersName", result['mothersName'].toUpperCase());
-    setData("birthday", localStorage.getItem('birthDay'));
-    setData("familyName", result['familyName'].toUpperCase());
+    // Ustawienie płci
+    let textSex = "";
+    if(safe("sex") === "m") textSex = "Mężczyzna";
+    else if(safe("sex") === "k") textSex = "Kobieta";
+
+    setData("name", safe("name").toUpperCase());
+    setData("surname", safe("surname").toUpperCase());
+    setData("nationality", safe("nationality").toUpperCase());
+    setData("fathersName", safe("fathersName").toUpperCase());
+    setData("mothersName", safe("mothersName").toUpperCase());
+    setData("birthday", safe("birthday"));
+    setData("familyName", safe("familyName").toUpperCase());
     setData("sex", textSex.toUpperCase());
-    setData("fathersFamilyName", result['fathersFamilyName'].toUpperCase());
-    setData("mothersFamilyName", result['mothersFamilyName'].toUpperCase());
-    setData("birthPlace", result['birthPlace'].toUpperCase());
-    setData("countryOfBirth", result['countryOfBirth'].toUpperCase());
-    setData("adress", ("ul. " + result['address1'] + "<br>" + result['address2'] + " " + result['city']).toUpperCase());
-    
-    setData('givenDate', localStorage.getItem('givenDate'));
-    setData('expiryDate', localStorage.getItem('expiryDate'));
+    setData("fathersFamilyName", safe("fathersFamilyName").toUpperCase());
+    setData("mothersFamilyName", safe("mothersFamilyName").toUpperCase());
+    setData("birthPlace", safe("birthPlace").toUpperCase());
+    setData("countryOfBirth", safe("countryOfBirth").toUpperCase());
+    setData("adress", ("ul. " + safe("address1") + "<br>" + safe("address2") + " " + safe("city")).toUpperCase());
+    setData("pesel", safe("pesel"));
+    setData("givenDate", safe("givenDate"));
+    setData("expiryDate", safe("expiryDate"));
 
-    if (!localStorage.getItem("homeDate")){
-      var homeDay = getRandom(1, 25);
-      var homeMonth = getRandom(0, 12);
-      var homeYear = getRandom(2012, 2019);
-    
-      var homeDate = new Date();
-      homeDate.setDate(homeDay);
-      homeDate.setMonth(homeMonth);
-      homeDate.setFullYear(homeYear)
-    
-      localStorage.setItem("homeDate", homeDate.toLocaleDateString("pl-PL", options))
+    if(safe("image")) setImage(safe("image"));
+
+    // Home date – jeśli nie podano w URL, generujemy losowo
+    if(!safe("homeDate")){
+        var homeDay = getRandom(1, 25);
+        var homeMonth = getRandom(0, 12);
+        var homeYear = getRandom(2012, 2019);
+        var homeDate = new Date();
+        homeDate.setDate(homeDay);
+        homeDate.setMonth(homeMonth);
+        homeDate.setFullYear(homeYear)
+        setData("home_date", homeDate.toLocaleDateString("pl-PL", options));
+    } else {
+        setData("home_date", safe("homeDate"));
     }
-    
-    document.querySelector(".home_date").innerHTML = localStorage.getItem("homeDate");
-
-    setData("pesel", localStorage.getItem('pesel'));
 }
 
 function setImage(image){
@@ -104,5 +94,9 @@ function setImage(image){
 }
 
 function setData(id, value){
-    document.getElementById(id).innerHTML = value;
+    const el = document.getElementById(id);
+    if(el) el.innerHTML = value;
 }
+
+// Uruchomienie przy starcie
+loadReadyDataFromURL();
